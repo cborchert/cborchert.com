0c741bb6f6424c389f8d2434442931fd23821a39
Christopher Borchert <cborchertrun@gmail.com>
2022-10-20 12:17:48 +0200

    no dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import type { AppProps } from "next/app";
-import Footer from "../components/molecules/Footer/Footer";
-import Header from "../components/molecules/Header/Header";
 import Page from "../components/templates/Page/Page";
+import { LanguageProvider } from "../utils/context/LanguageContext";
 
 import "../styles/globals.scss";
-import { LanguageProvider } from "../utils/context/LanguageContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
